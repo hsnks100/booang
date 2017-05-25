137b55283c6fa8f086abcb5a753e6740ca1dddd7
home-ubuntu <hsnks100@gmail.com>
2017-05-25 21:23:23 +0900

    add Makefile, then you just type "make"

--- Booang.cpp
+++ Booang.cpp
@@ -1,6 +1,7 @@
 // STL
 #include <iostream>                  // for std::cout
 
+
 // Boost
 #include <boost/graph/adjacency_list.hpp> // for customizable graphs
 #include <boost/graph/directed_graph.hpp> // A subclass to provide reasonable arguments to adjacency_list for a typical directed graph
