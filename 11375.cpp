83c1b75f548810ce011bc2576ee1da21f0c33587
HanSoo <hsnks100@gmail.com>
2017-07-21 16:56:10 +0900

    열혈강호 소스

---
+++ 11375.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include "Booang.hpp"
+using namespace std;
+
+int N, M;
+const int MAX_V = 1001;
+
+BGraph<int, int> graph;
+int visit[MAX_V];
+int backMatch[MAX_V];
+int dfs(int s){
+    if(visit[s]) return false;
+    visit[s] = true;
+    auto edges = graph[s];
+    for(auto& j : edges) {
+        int i = j.first;
+        if(backMatch[i] == -1 || dfs(backMatch[i])){
+            backMatch[i] = s;
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+
+
+    cin >> N >> M; 
+    for(int i=1; i<=N; i++){
+        int work;
+        scanf("%d", &work);
+        for(int j=1; j<=work; j++){
+            int w;
+            scanf("%d", &w);
+            graph.addVertex(i);
+            graph.addVertex(w); 
+            graph.addEdge(i, w);
+        }
+    }
+
+    graph.print();
+
+    int matchCnt = 0;
+    fill_n(backMatch, MAX_V, -1);
+    
+    for(int i=1; i<=N; i++){
+        fill_n(visit, MAX_V, false);
+        if(dfs(i)){
+            matchCnt++;
+        }
+    }
+    cout << matchCnt << endl;
+
+    return 0;
+    // 2016. 09. 16. (금) 03:22:50 KST
+}
+
