b06c97865c1eeea47713112b029f7bffb881570b
HanSoo <hsnks100@gmail.com>
2017-07-24 13:56:49 +0900

    update 11375

--- 11375.cpp
+++ 11375.cpp
@@ -5,50 +5,51 @@ using namespace std;
 int N, M;
 const int MAX_V = 1001;
 
-SimpleGraph graph;
+SimpleGraph G;
 int visit[MAX_V];
 int backMatch[MAX_V];
 int dfs(int s){
-    if(visit[s]) return false;
-    visit[s] = true;
-    auto edges = graph[s];
-    for(auto& j : edges) {
-        int i = j.first;
-        if(backMatch[i] == -1 || dfs(backMatch[i])){
-            backMatch[i] = s;
-            return true;
-        }
+  if(visit[s]) return false;
+  visit[s] = true;
+  auto edges = G[s];
+  for(auto& j : edges) {
+    int i = j.first;
+    if(backMatch[i] == -1 || dfs(backMatch[i])){
+      backMatch[i] = s;
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 int main(){
 
 
-    cin >> N >> M; 
-    for(int i=1; i<=N; i++){
-        int work;
-        scanf("%d", &work);
-        for(int j=1; j<=work; j++){
-            int w;
-            scanf("%d", &w);
-            graph.addVertex(i);
-            graph.addVertex(w); 
-            graph.addEdge(i, w);
-        }
-    }
 
-    graph.print(); 
-    int matchCnt = 0;
-    fill_n(backMatch, MAX_V, -1);
-    
-    for(int i=1; i<=N; i++){
-        fill_n(visit, MAX_V, false);
-        if(dfs(i)){
-            matchCnt++;
-        }
-    }
-    cout << matchCnt << endl;
 
-    return 0;
+  cin >> N >> M; 
+  G.resize(max(N, M) + 1);
+  for(int i=1; i<=N; i++){
+    int work;
+    scanf("%d", &work);
+    for(int j=1; j<=work; j++){
+      int w;
+      scanf("%d", &w);
+      G.addEdge(i, w);
+    }
+  }
+
+  G.print(); 
+  int matchCnt = 0;
+  fill_n(backMatch, MAX_V, -1);
+
+  for(int i=1; i<=N; i++){
+    fill_n(visit, MAX_V, false);
+    if(dfs(i)){
+      matchCnt++;
+    }
+  }
+  cout << matchCnt << endl;
+
+  return 0;
 }
 
