4ecb82673fc399e97f8f4a8c0d211261deebaf18
MusicianZem <201224510@pusan.ac.kr>
2017-08-31 18:59:17 +0900

    speed check by test data, but error something

---
+++ speed_check.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <cstdio>
+#include <math.h>
+
+#include "booang.hpp"
+using namespace std;
+using namespace boost;
+
+const int TESTCASE = 50;
+struct VertexProperty {
+    int Id;
+};
+int main() {
+    ifstream fin("test.txt");
+    clock_t startT = clock();
+    for (int t = 1; t <= TESTCASE; t++) {
+        BGraph<int, VertexProperty> G;
+
+        int nodeCount, edgeCount;
+        fin >> nodeCount >> edgeCount;
+
+        int startNode, endNode;
+        fin >> startNode >> endNode;
+
+        G.resize(nodeCount);
+        for (int i = 1; i <= edgeCount; i++) {
+            int fromN, toN, edgeWeight;
+            fin >> fromN >> toN >> edgeWeight;
+            G.addEdge(fromN - 1, toN - 1, edgeWeight);
+            G.addEdge(toN - 1, fromN - 1, edgeWeight);
+        }
+
+        auto ret = G.dijk(0);
+        /*for (auto it = ret.begin(); it != ret.end(); it++) {
+            cout << it->to << " " << it->weight << endl;
+        }*/
+    }
+    clock_t endT = clock();
+
+    printf("Booang : %f seconds \n", ((float)(endT - startT) / CLOCKS_PER_SEC));
+    fin.close();
+
+
+    ifstream fin2("test.txt");
+    // Boost
+    startT = clock();
+    for (int t = 1; t <= TESTCASE; t++) {
+        typedef adjacency_list < listS, vecS, directedS,
+            no_property, property < edge_weight_t, int > > graph_t;
+        typedef graph_traits < graph_t >::vertex_descriptor vertex_descriptor;
+        typedef std::pair<int, int> Edge;
+
+
+        int nodeCount, edgeCount;
+        fin2 >> nodeCount >> edgeCount;
+
+        int startNode, endNode;
+        fin2 >> startNode >> endNode;
+        edgeCount *= 2;
+
+        Edge *edge_arr = new Edge[edgeCount];
+        int *weights = new int[edgeCount];
+        int edgeC = edgeCount / 2;
+        for (int i = 0; i < edgeC; i++) {
+            int fromN, toN, edgeWeight;
+            fin2 >> fromN >> toN >> edgeWeight;
+            edge_arr[i] = Edge(fromN - 1, toN - 1);
+            edge_arr[i + 1] = Edge(toN - 1, fromN - 1);
+            weights[i] = weights[i + 1] = edgeWeight;
+        }
+
+        graph_t g(edge_arr, edge_arr + edgeCount, weights, nodeCount);
+        property_map<graph_t, edge_weight_t>::type weightmap = get(edge_weight, g);
+        vector<vertex_descriptor> p(num
+        vector<int> d(num_vertices(g));
+        vertex_descriptor s = vertex(0, g);
+        dijkstra_shortest_paths(g, s,
+            predecessor_map(boost::make_iterator_property_map(p.begin(), get(boost::vertex_index, g))).
+            distance_map(boost::make_iterator_property_map(d.begin(), get(boost::vertex_index, g))));
+        /*
+        cout << "digraph D {\n"
+            << "  rankdir=LR\n"
+            << "  size=\"4,3\"\n"
+            << "  ratio=\"fill\"\n"
+            << "  edge[style=\"bold\"]\n" << "  node[shape=\"circle\"]\n";
+
+        graph_traits < graph_t >::edge_iterator ei, ei_end;
+        for (boost::tie(ei, ei_end) = edges(g); ei != ei_end; ++ei) {
+            graph_traits < graph_t >::edge_descriptor e = *ei;
+            graph_traits < graph_t >::vertex_descriptor
+                u = source(e, g), v = target(e, g);
+            cout << u << " -> " << v
+                << "[label=\"" << get(weightmap, e) << "\"";
+            if (p[v] == u)
+                cout << ", color=\"black\"";
+            else
+                cout << ", color=\"grey\"";
+            cout << "]";
+        }
+        cout << "}";*/
+    }
+    endT = clock();
+
+    printf("Boost : %f seconds \n", ((float)(endT - startT) / CLOCKS_PER_SEC));
+}
