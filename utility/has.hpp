1c7bfb6bfd86689c2a3284fd7c83fb628dc37642
home-ubuntu <hsnks100@gmail.com>
2017-09-05 13:45:13 +0900

    has

---
+++ utility/has.hpp
@@ -0,0 +1,20 @@
+#ifndef __HAS__H_
+#define __HAS__H_
+namespace booang {
+    template <typename T>
+    class has_toString {
+    private:
+        typedef char Yes;
+        typedef Yes No[2];
+
+        template <typename U, U> struct really_has;
+        template <typename C> static Yes& Test(really_has <string(C::*)() const, &C::toString>*);
+        template <typename C> static Yes& Test(really_has <string(C::*)(), &C::toString>*);
+        template <typename> static No& Test(...);
+    public:
+        static bool const value = sizeof(Test<T>(0)) == sizeof(Yes);
+    };
+
+};
+
+#endif
